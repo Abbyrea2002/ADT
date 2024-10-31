e19b0b60cbb96faa38da64d9ebd6fe38eed2da5e
Abbyrea2002 <abbyrea2002@gmail.com>
2024-10-31 22:25:59 +0000

    push

---
+++ Animal/src/TestAnimal.java
@@ -0,0 +1,24 @@
+/**
+ * Created by abbyr on 31/10/2024
+ * COMMENTS ABOUT PROGRAM HERE
+ */
+public class TestAnimal
+{
+   public static void main(String[] args) {
+      // Create two Lion objects
+      Lion myLion1 = new Lion("vegetation", 10, 4, "Leo");    // Normal parameters
+      Lion myLion2 = new Lion(null, 0, 0, null);              // Empty parameters
+
+      // Set age of myLion1 to 3 (declared as 4 during object creation above)
+      myLion1.setAge(3);
+
+      // Print details of myLion1
+      System.out.println(myLion1);
+
+      // Print details of myLion2
+      System.out.println(myLion2);
+
+      // Print the number of Lion objects created
+      System.out.println(Lion.numberOfLions() + " lions have been created.");
+   }
+}//class
